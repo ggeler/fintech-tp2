e86f2e974ef3140bde7fbcc50071b036a425588b
gaston <ggeler@hotmail.com>
2022-03-25 14:01:26 -0300

    update

--- src/main/java/com/up/fintech/armagedon/tp2/tp2/swagger/CustomOpenApiUI.java
+++ src/main/java/com/up/fintech/armagedon/tp2/tp2/swagger/CustomOpenApiUI.java
@@ -20,8 +20,7 @@ public class CustomOpenApiUI {
 						 .license(new License().name("Licence Apache 2.0").url("https://www.apache.org/licenses/LICENSE-2.0")))
 				 .externalDocs(new ExternalDocumentation()
 						 .description("ISO 8583 Documentation")
-						 .url("https://en.wikipedia.org/wiki/ISO_8583"))
-				 .externalDocs(new ExternalDocumentation()
+						 .url("https://en.wikipedia.org/wiki/ISO_8583")
 						 .description("Json Documentation")
 						 .url("https://en.wikipedia.org/wiki/JSON"));
 	 }
