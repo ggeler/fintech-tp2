415a89b0989750f4ee772d7797e2e925cc31072b
gaston <ggeler@hotmail.com>
2022-06-02 19:40:22 -0300

    Cors

--- src/main/java/com/up/fintech/armagedon/misc/security/SecurityConfig.java
+++ src/main/java/com/up/fintech/armagedon/misc/security/SecurityConfig.java
@@ -22,6 +22,8 @@ public class SecurityConfig  {
 	@Bean
 	public SecurityFilterChain configure(HttpSecurity http) throws Exception {
 		http
+			.cors()
+		.and()
 			.authorizeRequests().antMatchers("/actuator/**","/v3/api-docs/**","/swagger-ui/**","/swagger-ui.html","/favicon.ico","/error").permitAll()
 			.and().authorizeRequests().antMatchers(HttpMethod.POST, "/fintech/external/bank/transfer/**").permitAll()
 			.and().csrf().ignoringAntMatchers("/fintech/external/bank/transfer/**")
